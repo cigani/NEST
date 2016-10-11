45e68b4db0789861f64ce1cdc0fd1f176c38fed4
Michael Jaquier <mj@tsf-460-wpa-7-109.epfl.ch>
2016-10-11 10:05:22 +0200

    set

---
+++ 25sFit.py
@@ -0,0 +1,101 @@
+import numpy as np
+import loading as l
+
+import sys
+
+from Experiment import *
+from GIF import *
+from AEC_Badel import *
+from AEC_Dummy import *
+from Filter_Rect_LinSpaced import *
+from Filter_Rect_LogSpaced import *
+from Filter_Exps import *
+
+import Tools
+
+
+PATH_FLAT = '/Users/mj/Documents/NEST/'
+sys.path.append(PATH_FLAT+'/GIFFittingToolbox/src')
+
+# import seaborn
+# import matplotlib.pyplot as plt
+
+
+
+
+####################################################
+# Import Data. Create Arrays
+####################################################
+
+
+dataArray = l.expLoad()[0]
+dataPath = l.expLoad()[1]
+V = [dataArray[n][0] for n,  k in enumerate(dataPath)]
+I = [dataArray[n][1] for n,  k in enumerate(dataPath)]
+V_units = 10**-3
+I_units = 10**-9
+
+
+####################################################
+# Load Training Set
+####################################################
+
+
+myExp = Experiment('Experiment 1',  0.1)
+myExp.addTrainingSetTrace(V[0], V_units, I[0], I_units, np.size(V[0])/10,
+                          FILETYPE='Array')
+myExp.addTestSetTrace(V[1], V_units, I[1], I_units, np.size(V[1])/10,
+                      FILETYPE='Array')
+
+
+####################################################
+# AEC
+####################################################
+
+
+myAEC = AEC_Dummy()
+myExp.setAEC(myAEC)
+myExp.performAEC()
+
+
+####################################################
+# GIF Fitting
+####################################################
+
+myGIF_rect = GIF(0.1)
+myGIF_rect.Tref = 4.0
+myGIF_rect.eta.setMetaParameters(length=5000.0, binsize_lb=2.0,
+                                 binsize_ub=1000.0, slope=4.5)
+myGIF_rect.fit(myExp, DT_beforeSpike=5.0)
+'''
+myGIF_exp = GIF(0.1)
+myGIF_exp.Tref = 4.0
+
+myGIF_exp.eta = Filter_Exps()
+myGIF_exp.eta.setFilter_Timescales([1.0,  5.0,30.0,100.0,500.0])
+
+#Perform Fit
+myGIF_exp.fit(myExp,  DT_beforeSpike = 5.0)
+'''
+
+####################################################
+# Compare Rect and Exp Models
+####################################################
+
+myPredictionGIF_rect = myExp.predictSpikes(myGIF_rect,  nb_rep=500)
+# myPredictionGIF_exp = myExp.predictSpikes(myGIF_exp,  nb_rep=500)
+
+
+print "Model Performance"
+print "GIF Rect: "
+myPredictionGIF_rect.computeMD_Kistler(40.0,  0.1)
+print "GIF exp: "
+# myPredictionGIF_exp.computeMD_Kistler(4.0,  0.1)
+
+
+####################################################
+# Compare model Parameters
+####################################################
+
+
+# GIF.compareModels([myGIF_rect,  myGIF_exp], labels['GIF rect', 'GIF exp'])
---
+++ loading.py
@@ -0,0 +1,42 @@
+import numpy as np
+import h5py
+import glob
+
+
+# Import Files
+
+
+dataPath = glob.glob('./data/*.dat')
+assert dataPath is not None
+
+expData = [[[], []] for x in xrange(np.size(dataPath))]
+
+
+def expLoad(dataPath=dataPath, cols=1,
+            ExpStart=7000, ExpStop=280000,
+            iStart=0, iE=0.6):
+    expData = [[[], []] for x in xrange(np.size(dataPath))]
+    for i, k in enumerate(dataPath):
+        expFile = np.loadtxt(k, usecols=[cols])
+
+        if iStart is not 0:
+            expData[i] = np.array([expFile,
+                                   np.append(
+                                       np.zeros(iStart),
+                                       np.ones(np.size(
+                                           expFile[iE:ExpStop]))*iE)])
+        else:
+            expData[i] = np.array([
+                expFile,
+                np.ones(np.size(expFile[ExpStart:ExpStop]))*0.6])
+    return expData, dataPath
+
+
+dataArray = expLoad()[0]
+data2 = expLoad()[1]
+print data2
+
+V = [dataArray[n][0] for n, k in enumerate(dataPath)]
+I = [dataArray[n][1] for n, k in enumerate(dataPath)]
+V_units = 10**-3
+I_units = 10**-9
