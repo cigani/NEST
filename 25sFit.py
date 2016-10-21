a30f359793f3c1545e494f810d7f78f4e17180c9
Michael Jaquier <shibasu@gmail.com>
2016-10-21 23:13:31 +0200

    close

--- 25sFit.py
+++ 25sFit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import glob
-# import loading as l
+import h5py
 
 import sys
 PATH_FLAT = '/Users/mj/Documents/NEST/'
@@ -21,27 +21,12 @@ import Tools
 # import matplotlib.pyplot as plt
 
 
-
-
 ####################################################
 # Import Data. Create Arrays
 ####################################################
 
-EXPERIMENT_PATH = '/L5_TTPC1_cADpyr232_1/python_recordings/Data/*.dat'
+EXPERIMENT_PATH = 'L5_TTPC1_cADpyr232_1/python_recordings/Data/H5Data/*.hdf5'
 DATA_PATH = glob.glob(PATH_FLAT+EXPERIMENT_PATH)
-
-"""
-
-dataArray = l.expLoad()[0]
-dataPath = l.expLoad()[1]
-V = [dataArray[n][0] for n, k in enumerate(dataPath)]
-I = [dataArray[n][1] for n, k in enumerate(dataPath)]
-V_units = 10**-3
-I_units = 10**-9
-
-assert np.size(I) == np.size(V) # These need to be equal
-
-"""
 
 V_units = 10**-3
 I_units = 10**-9
@@ -49,12 +34,25 @@ I_units = 10**-9
 V = []
 I = []
 
+for n in DATA_PATH:
+    h5Data = h5py.File(n, 'r')
+
+for n, k in zip(h5Data.get('current'), h5Data.get('voltage')):
+    Itemp = np.array(n)
+    I.append(Itemp)
+    Vtemp = np.array(k)
+    V.append(Vtemp)
+
+assert(np.size(V) == np.size(I))
+
+
+'''
 for i, k in enumerate(DATA_PATH):
     tempV = np.loadtxt(k, usecols=[0])
     tempA = np.loadtxt(k, usecols=[1])
     V.append(tempV)
     I.append(tempA)
-
+'''
 
 ####################################################
 # Load Training Set
@@ -62,16 +60,21 @@ for i, k in enumerate(DATA_PATH):
 
 
 myExp = Experiment('Experiment 1', 0.1)
-myExp.addTrainingSetTrace(V[0], V_units,
-                            I[0], I_units,
-                            np.size(V[1])/10,
-                            FILETYPE='Array')
-for n,k in enumerate(DATA_PATH[1:]):
-                     myExp.addTestSetTrace(V[n], V_units,
-                                           I[n], I_units,
-                                           np.size(V[n])/10,
-                                           FILETYPE='Array')
 
+for n, k in enumerate(V[:1]):
+    myExp.addTrainingSetTrace(V[n], V_units, I[n], I_units, np.size(V[n])/10,
+                              FILETYPE='Array')
+    myExp.trainingset_traces[0].setROI([[7798,200000.0]])
+
+
+for n, k in enumerate(V[1:]):
+    volt = V[n]
+    amp = I[n]
+    time = np.size(volt)/10
+    myExp.addTestSetTrace(volt, V_units, amp, I_units, time, FILETYPE='Array')
+
+# myExp.plotTrainingSet()
+# myExp.plotTestSet()
 
 ####################################################
 # AEC
@@ -81,6 +84,7 @@ for n,k in enumerate(DATA_PATH[1:]):
 myAEC = AEC_Dummy()
 myExp.setAEC(myAEC)
 myExp.performAEC()
+
 '''
 myExp.setAECTrace(V[0], V_units,
                             I[0], I_units,
@@ -100,23 +104,11 @@ myExp.performAEC()
 ####################################################
 # GIF Fitting
 ####################################################
-myGIF = GIF(0.1)
-
-# Define parameters
-myGIF.Tref = 4.0
-
-myGIF.eta = Filter_Rect_LogSpaced()
-myGIF.eta.setMetaParameters(length=200.0, binsize_lb=0.1, binsize_ub=2.0, slope=30.0)
 
 
-myGIF.gamma = Filter_Rect_LogSpaced()
-myGIF.gamma.setMetaParameters(length=1.0, binsize_lb=0.1, binsize_ub=1.0, slope=90.0)
-# myAEC.K_opt.setMetaParameters(length=150.0, binsize_lb=myExp.dt, binsize_ub=2.0, slope=30.0, clamp_period=1.0)
-# Define the ROI of the training set to be used for the fit (in this example we will use only the first 100 s)
-#myExp.trainingset_traces[0].setROI([[0,100000.0]])
-
+'''
 # To visualize the training set and the ROI call again
-#myExp.plotTrainingSet()
+# myExp.plotTrainingSet()
 
 # Perform the fit
 myGIF.fit(myExp, DT_beforeSpike=5.0)
@@ -125,8 +117,6 @@ myGIF.fit(myExp, DT_beforeSpike=5.0)
 # myGIF.printParameters()
 # myGIF.plotParameters()
 
-
-'''
 myGIF_rect = GIF(0.1)
 myGIF_rect.Tref = 4.0
 myGIF_rect.eta.setMetaParameters(length=2000.0, binsize_lb=2.0,
@@ -134,24 +124,20 @@ myGIF_rect.eta.setMetaParameters(length=2000.0, binsize_lb=2.0,
 myGIF_rect.gamma = Filter_Rect_LogSpaced()
 myGIF_rect.gamma.setMetaParameters(length=1000.0, binsize_lb=5.1,
                                    binsize_ub=1000.0, slope=5.0)
-myGIF_rect.fit(myExp, DT_beforeSpike=2.0)
-
-'''
+myGIF_rect.fit(myExp, DT_beforeSpike=5.0)
 
 
-'''
+myGIF_exp = GIF(0.1)
+myGIF_exp.Tref = 4.0
 
-# myGIF_exp = GIF(0.1)
-# myGIF_exp.Tref = 4.0
+myGIF_exp.eta = Filter_Exps()
+myGIF_exp.eta.setFilter_Timescales([0.01, 0.1, 1.0,
+                                    5.0, 30.0, 100.0,
+                                    200.0, 500.0])
 
-# myGIF_exp.eta = Filter_Exps()
-# myGIF_exp.eta.setFilter_Timescales([0.01, 0.1, 1.0,
-#                                     5.0, 30.0, 100.0,
-#                                     200.0, 500.0])
-
-#Perform Fit
+# Perform Fit
 # myGIF_exp.fit(myExp, DT_beforeSpike=5.0)
-
+'''
 # Create a new object GIF
 myGIF = GIF(0.1)
 
@@ -159,7 +145,7 @@ myGIF = GIF(0.1)
 myGIF.Tref = 4.0
 
 myGIF.eta = Filter_Rect_LogSpaced()
-myGIF.eta.setMetaParameters(length=2000.0, binsize_lb=2.0,
+myGIF.eta.setMetaParameters(length=1000.0, binsize_lb=0.1,
                             binsize_ub=1000.0, slope=4.5)
 
 myGIF.gamma = Filter_Rect_LogSpaced()
@@ -167,30 +153,31 @@ myGIF.gamma.setMetaParameters(length=1000.0, binsize_lb=5.0,
                               binsize_ub=1000.0, slope=5.0)
 
 # Perform the fit
-myGIF.fit(myExp, DT_beforeSpike=2.0)
-'''
+myGIF.fit(myExp, DT_beforeSpike=5.0)
+
+
 ####################################################
 # Compare Rect and Exp Models
 ####################################################
 
 # myPredictionGIF_rect = myExp.predictSpikes(myGIF_rect, nb_rep=500)
 # myPredictionGIF_exp = myExp.predictSpikes(myGIF_exp, nb_rep=500)
-# myPredictionGIF_rect.plotRaster(delta=100)
+# myPredictionGIF_rect.plotRaster(delta=1000)
 
-print "Model Performance"
+# print "Model Performance"
 print "GIF Rect: "
 # myPredictionGIF_gene = myExp.predictSpikes(myGIF, nb_rep=500)
-# myPredictionGIF_rect.computeMD_Kistler(40.0, 0.1)
+# myPredictionGIF_rect.computeMD_Kistler(100.0, 0.1)
 # print "GIF exp: "
-# myPredictionGIF_exp.computeMD_Kistler(4.0,  0.1)
-# myPredictionGIF_gene.computeMD_Kistler(4.0, 0.1)
-myPrediction = myExp.predictSpikes(myGIF, nb_rep=500)
-
+# myPredictionGIF_exp.computeMD_Kistler(100.0,  0.1)
+Prediction = myExp.predictSpikes(myGIF, nb_rep=500)
+Prediction.computeMD_Kistler(4.0, 0.1)
+m
 # Compute Md* with a temporal precision of +/- 4ms
-Md = myPrediction.computeMD_Kistler(4.0, 0.1)
+# Md = myPrediction.computeMD_Kistler(4.0, 0.1)
 
 # Plot data vs model prediction
-myPrediction.plotRaster(delta=1000.0)
+# myPrediction.plotRaster(delta=1000.0)
 ####################################################
 # Compare model Parameters
 ####################################################
