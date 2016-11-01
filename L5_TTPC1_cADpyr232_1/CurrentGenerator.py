c21f648d2597e07b584e69c305de20d02e6e2e81
Michael Jaquier <shibasu@gmail.com>
2016-11-01 17:51:30 +0100

    latest

--- L5_TTPC1_cADpyr232_1/CurrentGenerator.py
+++ L5_TTPC1_cADpyr232_1/CurrentGenerator.py
@@ -1,28 +1,46 @@
 import numpy as np
+from scipy import weave
 
 
-class CurrentGenerator():
+class CurrentGenerator:
 
-    def __init__(self):
+    def __init__(self,
+                 seed=777,
+                 time=3000,
+                 tau=100,
+                 i_e0=0.0,
+                 sigmaMax=0.8,
+                 sigmaMin=0.5,
+                 frequency=0.2,
+                 dt=0.1,
+                 subthresholdvoltage=[],
+                 threshold=0.0,
+                 optsigma = 0.5):
 
-        self.seed = np.random.seed(777)
-        self.time = 10000
-        self.tau = 100.0
-        self.i_e0 = 0.48
+        self.seed = np.random.seed(seed)
+        self.time = time
+        self.tau = tau
+        self.i_e0 = i_e0
         self.i_e = []
-        self.sigma0 = 0.48
-        self.sigma = 0.5
+        self.sigmaMax = sigmaMax
+        self.sigmaMin = sigmaMin
+        self.sigma = (self.sigmaMax+self.sigmaMin)/2
+        self.deltsigma = (self.sigmaMax-self.sigmaMax)/(2*self.sigma)
         self.variance = []
         self.duration = 0.0
-        self.frequency = 0.2
-        self.dt = 0.1
+        self.frequency = frequency
+        self.dt = dt
+        self.subthresholdVoltage = subthresholdvoltage
+        self.tolerance = 0.2
+        self.spks = 0
+        self.spks_flag = False
+        self.threshold = threshold
+        self.optsigma = optsigma
 
     def generatecurrent(self):
-
         timespan = np.arange(self.time)
-
         self.i_e = np.zeros(self.time)
-        self.variance = self.sigma0*(1+self.sigma*np.sin(
+        self.variance = self.sigma*(1+self.deltsigma*np.sin(
                 2*np.pi*timespan*self.frequency*10**-3))
         for n in np.arange(self.time - 1):
             self.i_e[n+1] = self.i_e[n] + \
@@ -31,12 +49,92 @@ class CurrentGenerator():
                                                 2) * self.dt)/ self.tau) * \
                             np.random.normal()
             self.i_e[n] = self.i_e[n+1]
-        self.plotcurrent()
+        #self.plotcurrent()
+        return self.i_e
+
+    def subthresholdVar(self):
+        selection = self.getFarFromSpikes()
+        subthresholdVariance = np.var(self.subthresholdVoltage[selection])
+        print('Subthresh Var: ')
+        print subthresholdVariance
+
+        return subthresholdVariance
+
+    def optgeneratecurrent(self):
+        timespan = np.arange(self.time)
+        print "self.time: "
+        print self.time
+        self.i_e = np.zeros(self.time)
+        self.variance = self.optsigma*(1+0.5*np.sin(
+            2*np.pi*timespan*self.frequency*10**-3))
+        for n in np.arange(self.time - 1):
+            self.i_e[n+1] = self.i_e[n] + \
+                            ((self.i_e0-self.i_e[n])/self.tau) * self.dt + \
+                            np.sqrt((2*np.power(self.variance[n],
+                                                2) * self.dt)/ self.tau) * \
+                            np.random.normal()
+            self.i_e[n] = self.i_e[n+1]
         return self.i_e
 
     def plotcurrent(self):
         import pylab
-        pylab.plot(self.i_e)
+        pylab.plot(self.variance)
         pylab.show()
+
+    def detectSpikes(self, ref=3.0):
+
+        """
+        Detect action potentials by threshold crossing (parameter threshold,
+        mV) from below (i.e. with dV/dt>0).
+        To avoid multiple detection of same spike due to noise, use an
+        'absolute refractory period' ref, in ms.
+        """
+
+        self.spks = []
+        ref_ind = int(ref / self.dt)
+        t = 0
+        while (t < len(self.subthresholdVoltage) - 1):
+
+            if (self.subthresholdVoltage[t] >= self.threshold >=
+                    self.subthresholdVoltage[t -1]):
+                self.spks.append(t)
+                t += ref_ind
+            t += 1
+
+        self.spks = np.array(self.spks)
+        self.spks_flag = True
+
+    def getFarFromSpikes(self, DT_before=5.0, DT_after=5.0):
+
+        """
+        Return indices of the trace which are in ROI. Exclude all datapoints
+        which are close to a spike.
+        DT_before: ms
+        DT_after: ms
+        These two parameters define the region to cut around each spike.
+        """
+        if not self.spks_flag:
+            self.detectSpikes()
+
+        L = len(self.subthresholdVoltage)
+
+        LR_flag = np.ones(L)
+
+        LR_flag[:] = 0.0
+
+        # Remove region around spikes
+        DT_before_i = int(DT_before / self.dt)
+        DT_after_i = int(DT_after / self.dt)
+
+        for s in self.spks:
+            lb = max(0, s - DT_before_i)
+            ub = min(L, s + DT_after_i)
+
+            LR_flag[lb: ub] = 1
+
+        indices = np.where(LR_flag == 0)[0]
+
+        return indices
+
 
 CurrentGenerator().generatecurrent()
--- L5_TTPC1_cADpyr232_1/run2.py
+++ L5_TTPC1_cADpyr232_1/run2.py
@@ -7,141 +7,209 @@ import numpy as np
 import CurrentGenerator
 
 
-def create_cell(add_synapses=True):
-    """Create the cell model"""
-    # Load morphology
-    neuron.h.load_file("morphology.hoc")
-    # Load biophysics
-    neuron.h.load_file("biophysics.hoc")
-    # Load main cell template
-    neuron.h.load_file("template.hoc")
+class Simulator:
 
-    # Instantiate the cell from the template
+    def __init__(self):
+        self.sigmaMinFlag = True
+        self.sigmaMaxFlag = True
+        self.recordings = []
+        self.cell = []
+        self.stimuli = []
+        self.smax = 0.0
+        self.smin = 0.0
+        self.sigmaopt = 0.0
+        self.playVector = []
+        self.current = []
+        self.currentFlag = True
 
-    print("Loading cell cADpyr232_L5_TTPC1_0fb1ca4724")
-    cell = neuron.h.cADpyr232_L5_TTPC1_0fb1ca4724(1 if add_synapses else 0)
-    return cell
+    def create_cell(self, add_synapses=True):
+        """Create the cell model"""
+        # Load morphology
+        neuron.h.load_file("morphology.hoc")
+        # Load biophysics
+        neuron.h.load_file("biophysics.hoc")
+        # Load main cell template
+        neuron.h.load_file("template.hoc")
 
+        # Instantiate the cell from the template
 
-def create_stimuli(cell):
-    """Create the stimuli"""
-
-    print('Attaching stimulus electrodes')
-
-    stimuli = []
-
-    iclamp = neuron.h.IClamp(0.5, sec=cell.soma[0])
-    iclamp.delay = 0
-    iclamp.dur = 1e9
-
-    stimuli.append(iclamp)
-
-    return stimuli
-
-
-def create_recordings(cell, stim):
-    """Create the recordings"""
-    print('Attaching recording electrodes')
-
-    recordings = {'time': neuron.h.Vector(), 'soma(0.5)': neuron.h.Vector(),
-                  'current': neuron.h.Vector()}
-
-    recordings['current'].record(stim._ref_amp, 0.1)
-    recordings['time'].record(neuron.h._ref_t, 0.1)
-    recordings['soma(0.5)'].record(cell.soma[0](0.5)._ref_v, 0.1)
-
-    return recordings
+        print("Loading cell cADpyr232_L5_TTPC1_0fb1ca4724")
+        cell = neuron.h.cADpyr232_L5_TTPC1_0fb1ca4724(1 if add_synapses else 0)
+        return cell
 
 
-def run_step(plot_traces=None):
-    """Run step current simulation with index step_number"""
+    def create_stimuli(self, cell):
+        """Create the stimuli"""
 
-    cell = create_cell(add_synapses=False)
-    stimuli = create_stimuli(cell)
-    recordings = create_recordings(cell, stimuli[0])
-    print('Setting simulation time to 3s for the step currents')
-    neuron.h.tstop = 10000
+        print('Attaching stimulus electrodes')
 
-    print('Disabling variable timestep integration')
-    neuron.h.cvode_active(0)
+        stimuli = []
 
-    inputNoisy = generatecurrent(0.000002, (neuron.h.tstop / neuron.h.dt + 1))
+        iclamp = neuron.h.IClamp(0.5, sec=cell.soma[0])
+        iclamp.delay = 0
+        iclamp.dur = 1e9
 
-    VecStim = neuron.h.Vector(np.size(inputNoisy))
-    for k in xrange(np.size(inputNoisy)):  # Hacky but stops seg errors
-        VecStim.set(k, inputNoisy[k])
-        # print VecStim.get(k)
+        stimuli.append(iclamp)
 
-    VecStim.play(stimuli[0]._ref_amp, neuron.h.dt)
-    print('Running for %f ms' % neuron.h.tstop)
-    #neuron.h.run()
-
-    time = np.array(recordings['time'])
-    soma_voltage = np.array(recordings['soma(0.5)'])
-    soma_current = np.array(recordings['current'])
-    recordings_dir = 'python_recordings'
-
-    import pylab
-    pylab.plot(soma_current)
-    pylab.show()
-
-    soma_voltage_filename = os.path.join(
-        recordings_dir,
-        'soma_voltage_step.dat')
-    np.savetxt(
-            soma_voltage_filename,
-            np.transpose(
-               np.vstack((
-                   time,
-                   soma_voltage,
-                   soma_current))))
-
-    print('Soma voltage saved to: %s'
-          % soma_voltage_filename)
-
-    if plot_traces:
-        import pylab
-        pylab.figure()
-        pylab.plot(recordings['time'], recordings['soma(0.5)'])
-        pylab.xlabel('time (ms)')
-        pylab.ylabel('Vm (mV)')
-    #import h5Xchange
-    #hfpy = h5Xchange
-    #print hfpy
+        return stimuli
+    def create_current(self, smax=0.8, smin=0.5, time=3000, i_e0=0.0):
+        cg = CurrentGenerator.CurrentGenerator(time=time,
+                                               i_e0=i_e0,
+                                               sigmaMax=smax,
+                                               sigmaMin=smin)
+        self.current = cg.generatecurrent()
+        self.currentFlag = False
 
 
-def init_simulation():
-    """Initialise simulation environment"""
 
-    neuron.h.load_file("stdrun.hoc")
-    neuron.h.load_file("import3d.hoc")
+    def create_recordings(self, cell, stim):
+        """Create the recordings"""
+        print('Attaching recording electrodes')
 
-    print('Loading constants')
-    neuron.h.load_file('constants.hoc')
+        self.recordings = {'time': neuron.h.Vector(), 'soma(0.5)':
+            neuron.h.Vector(),
+                      'current': neuron.h.Vector()}
+
+        self.recordings['current'].record(stim._ref_amp, 0.1)
+        self.recordings['time'].record(neuron.h._ref_t, 0.1)
+        self.recordings['soma(0.5)'].record(cell.soma[0](0.5)._ref_v, 0.1)
+
+        return self.recordings
 
 
-def main(plot_traces=True):
-    """Main"""
+    def run_step(self, smax=0.8, smin=0.3,
+                 sigmaMaxFlag=True, sigmaMinFlag=True,
+                 optsigma=0.0, voltage=[], time=3000, optFlag=False,
+                 i_e0=0.0):
+        """Run step current simulation with index step_number"""
 
-    # Import matplotlib to plot the traces
-    if plot_traces:
-        import matplotlib
-        matplotlib.rcParams['path.simplify'] = False
+        self.cell = self.create_cell(add_synapses=False)
+        self.stimuli = self.create_stimuli(self.cell)
+        self.recordings = self.create_recordings(self.cell, self.stimuli[0])
 
-    init_simulation()
+        neuron.h.tstop = time
+        neuron.h.cvode_active(0)
 
-    run_step(plot_traces=plot_traces)
+        if not optFlag:
+            if not self.currentFlag:
+                inputNoisy = self.current
+                VecStim = neuron.h.Vector(np.size(inputNoisy))
+            else:
+                self.create_current(smin=smin, smax=smax, i_e0=i_e0)
+                inputNoisy = self.current
+                VecStim = neuron.h.Vector(np.size(inputNoisy))
+        else:
+            pass
 
-    if plot_traces:
-        import pylab
-        pylab.show()
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        main(plot_traces=True)
-    elif len(sys.argv) == 2 and sys.argv[1] == '--no-plots':
-        main(plot_traces=False)
-    else:
-        raise Exception(
-            "Script only accepts one argument: --no-plots, not %s" %
-            str(sys.argv))
+        for k in xrange(np.size(inputNoisy)):  # Hacky but stops seg errors
+            VecStim.set(k, inputNoisy[k])
+            # print VecStim.get(k)
+
+        VecStim.play(self.stimuli[0]._ref_amp, neuron.h.dt)
+        print('Running for %f ms' % neuron.h.tstop)
+        neuron.h.run()
+        time = np.array(self.recordings['time'])
+        soma_voltage = np.array(self.recordings['soma(0.5)'])
+        soma_current = np.array(self.recordings['current'])
+        recordings_dir = 'python_recordings'
+        if (sigmaMaxFlag or sigmaMinFlag):
+            print "Flags"
+            print sigmaMaxFlag
+            print sigmaMinFlag
+            self.optimize(time= neuron.h.tstop,
+                     optsigma=optsigma,
+                     voltage=soma_voltage)
+
+        soma_voltage_filename = os.path.join(
+            recordings_dir,
+            'soma_voltage_step.dat')
+        np.savetxt(
+                soma_voltage_filename,
+                np.transpose(
+                   np.vstack((
+                       time,
+                       soma_voltage,
+                       soma_current))))
+
+        print('Soma voltage saved to: %s'
+              % soma_voltage_filename)
+        plot_traces=False
+        if plot_traces:
+            import pylab
+            pylab.figure()
+            pylab.plot(self.recordings['time'], self.recordings['soma(0.5)'])
+            pylab.xlabel('time (ms)')
+            pylab.ylabel('Vm (mV)')
+
+    def optimize(self, time, voltage, optsigma=0.0, sigmaMaxFlag=True,
+                 sigmaMinFlag=True):
+        sigmaMaxFlag = sigmaMaxFlag
+        sigmaMinFlag = sigmaMinFlag
+        smax = 0.1
+        smin = 0.1
+        optsigma=optsigma
+
+        os = CurrentGenerator.CurrentGenerator(time=time,
+                                               subthresholdvoltage=voltage)
+        tempSig = os.subthresholdVar()
+
+        if 0.6 < tempSig > 0.8:
+            sigmaMaxFlag=False
+            print "Sigma Max: "
+            print optsigma
+
+
+        if sigmaMaxFlag:
+            print "tempSig"
+            print tempSig
+            if 0.8 >= tempSig >= 0.6:
+                sigmaMaxFlag = False
+                print "Proper smax: "
+                print optsigma
+                run_step(smax=optsigma, optsigma=optsigma,
+                         sigmaMaxFlag=sigmaMaxFlag)
+            elif tempSig < 0.6:
+                optsigma -= 0.1
+                run_step(optsigma=optsigma)
+            elif tempSig > 0.8:
+                optsigma += 0.1
+                run_step(optsigma=optsigma)
+
+        if sigmaMinFlag:
+            tempSig = os.optimizeSigma()
+            print "tempSig"
+            print tempSig
+            if 0.4 >= tempSig >= 0.2:
+                sigmaMinFlag = False
+                print "Proper smin: "
+                print optsigma
+                run_step(smax=smax, smin=smin,
+                         sigmaMaxFlag=sigmaMaxFlag, sigmaMinFlag=sigmaMinFlag)
+            elif tempSig < 0.2:
+                smin += 0.1
+                run_step(smax=smax, smin=smin,
+                         sigmaMaxFlag=sigmaMaxFlag, sigmaMinFlag=sigmaMinFlag)
+            elif tempSig > 0.4:
+                smin -= 0.1
+                run_step(smax=smax, smin=smin,
+                         sigmaMaxFlag=sigmaMaxFlag, sigmaMinFlag=sigmaMinFlag)
+
+    def init_simulation(self):
+        """Initialise simulation environment"""
+
+        neuron.h.load_file("stdrun.hoc")
+        neuron.h.load_file("import3d.hoc")
+
+        print('Loading constants')
+        neuron.h.load_file('constants.hoc')
+
+
+    def main(self, plot_traces=True):
+
+        """Main"""
+
+        self.init_simulation()
+
+        self.run_step()
+
